67403a1d2d28eb429e73304d28bf41f51bc0d059
sirinen <48890122+sirinen@users.noreply.github.com>
2019-11-23 15:16:39 +0100

    Create Esp2.ino

---
+++ lab/3/Report/2/Esp2.ino
@@ -0,0 +1,17 @@
+Code for ESP32 for being in Master mode :
+
+#include <Wire.h>
+// Include the required Wire library for I2C<br>#include 
+int x = 0;
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
+}
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  x++; // Increment x
+  if (x > 5) x = 0; // `reset x once it gets 6
+  delay(500);
+}
